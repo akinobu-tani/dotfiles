60a595d17b4f05137fdcd41ed210f4704e1fa6dc
Akinobu Tani <akinobu.x.tani@gmail.com>
2017-04-20 15:51:12 +0900

    rbenvのインストールを追加

---
+++ etc/rbenv/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ex
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/ianheggie/rbenv-binstubs.git ~/.rbenv/plugins/rbenv-binstubs
