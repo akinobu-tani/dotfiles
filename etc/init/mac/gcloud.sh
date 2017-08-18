397ac7b506142f63516611a6c4278a7b4f4499cf
Akinobu Tani <akinobu.x.tani@gmail.com>
2017-08-19 08:15:59 +0900

    install google cloud SDK

---
+++ etc/init/mac/gcloud.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Stop script if errors occur
+trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
+set -e
+
+. ${HOME}/dotfiles/etc/include.sh
+
+if has "gcloud"; then
+  echo "gcloud: already installed"
+  exit
+fi
+
+curl https://sdk.cloud.google.com | bash --disable-prompts
+if ! has "gcloud"; then
+    echo "error: gcloud: failed to install"
+    exit 1
+fi
+
+echo "gcloud: installed successfully"
