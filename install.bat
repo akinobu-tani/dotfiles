de3319f8c185d3e560197e1bc43e642775e3380a
Akinobu tani <akinobu.x.tani@gmail.com>
2022-04-25 16:04:39 +0900

    change path

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
 
-mklink /d "%APPDATA%\Sublime Text 3\Packages\User" "%HOMEPATH%\dotfiles\etc\sublime\windows\User"
+mklink /d "%APPDATA%\Sublime Text\Packages\User" "%HOMEPATH%\dotfiles\etc\sublime\windows\User"
 mklink /d "%HOMEPATH%\bin" "%HOMEPATH%\dotfiles\windows\bin"
